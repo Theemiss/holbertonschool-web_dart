09f03d96551ee6a0ec0d5f8642db921b37bce22d
Theemiss <midinfotn401@gmail.com>
2022-05-25 15:47:43 +0100

    changed function  name

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -1,4 +1,4 @@
-String sumFunc(int a, int b){
+String showFunc(int a, int b){
 return "Add ${a.toString()} + ${b.toString()} = ${add(a,b)}\nSub ${a.toString()} - ${b.toString()} = ${sub(a,b)}";
 }
 int add(int a, int b){
