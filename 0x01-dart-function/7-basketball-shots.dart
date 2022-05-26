04551ac519b45ccfb8129c0bd705e611b09573a1
Theemiss <midinfotn401@gmail.com>
2022-05-26 11:43:44 +0100

    Task 8 Test

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,8 +1,8 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   int teamAscore =
-      teamA['Free throws']! + teamA['2 pointer']! * 2 + teamA['3 pointer']! * 3;
+      teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
   int teamBscore =
-      teamB['Free throws']! + teamB['2 pointer']! * 2 + teamB['3 pointer']! * 3;
+      teamB['Free throws']! + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
       if(teamAscore > teamBscore){
         return 1;
       }
